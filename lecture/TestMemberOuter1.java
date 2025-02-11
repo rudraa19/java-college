b0c535978b50628a5f0c0017f71651219441b271
rudraa19 <rudra19206@gmail.com>
2025-02-11 06:09:46 +0000

    Upload: Files to GitHub

---
+++ lecture/TestMemberOuter1.java
@@ -0,0 +1,12 @@
+public class TestMemberOuter1 {
+  private int data = 30;
+  class inner{
+    void msg(){System.out.println("data is "+data);}
+  }
+  public static void main(String[] args) {
+    TestMemberOuter1 obj = new TestMemberOuter1();
+    TestMemberOuter1.inner in = obj.new inner();
+
+    in.msg();
+  }
+}
